1bfd81efd6622b93123353a4678b86344161148b
enovak89 <enovak89@mail.ru>
2023-01-13 12:26:57 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+    byte a = 1;
+    short b = 150;
+    int c = 30000;
+    long d = 1000_000_000;
+    float e = 3.14f;
+    double f = 6.28;
+    System.out.println("Значение переменной a с типом byte равно " + a);
+    System.out.println("Значение переменной b с типом byte равно " + b);
+
+
+
+    }
+}
