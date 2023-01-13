fb96f242f15050389c88a05f440d27cba63d6797
enovak89 <enovak89@mail.ru>
2023-01-13 12:34:18 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-    byte a = 1;
-    short b = 150;
-    int c = 30000;
-    long d = 1000_000_000;
-    float e = 3.14f;
-    double f = 6.28;
-    System.out.println("Значение переменной a с типом byte равно " + a);
-    System.out.println("Значение переменной b с типом byte равно " + b);
+    task1();
+    }
 
-
-
+    public static void task1 () {
+        byte a = 1;
+        short b = 150;
+        int c = 30000;
+        long d = 1000_000_000;
+        float e = 3.14f;
+        double f = 6.28;
+        System.out.println("Задача 1" + "\n" + "Значение переменной a с типом byte равно " + a);
+        System.out.println("Значение переменной b с типом short равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
     }
 }
